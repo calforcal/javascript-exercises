e07451191964349f2337959ce18965485c95fcf2
calforcal <mcalla242@gmail.com>
2023-01-27 15:42:45 -0700

    Upload the solution for 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeated = "";
+
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            repeated = repeated.concat(string);
+        }
+        return repeated;        
+    }
+    else {
+        repeated = "ERROR";
+        return repeated;
+    }
 
 };
 
