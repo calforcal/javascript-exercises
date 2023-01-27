d601c797e0ee2eb0a418b067f778f1d7d2d3dd3d
calforcal <mcalla242@gmail.com>
2023-01-27 16:31:49 -0700

    Upload the solution for 03-reverse-string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
-const reverseString = function() {
+/*
+Get string input for function reverseString
+Reverse the String
+    Use for loop to iterate through each letter in the string
+        Starting with string index equal to length - 1.
+        Slice last character and set index equal to 0 or (i).
+*/
+
+
+const reverseString = function(string) {
+    let array = string.split("");
+    let reversedArray = array.reverse();
+
+    let reversedString = reversedArray.join("");
+
+    return reversedString;
 
 };
 
