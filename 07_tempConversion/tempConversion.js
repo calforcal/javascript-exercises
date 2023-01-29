ac8b396c86197867e1483734f68391a81c0e9018
calforcal <mcalla242@gmail.com>
2023-01-29 14:17:03 -0700

    Upload 07-tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+
+  let converted = ((farenheit - 32) * (5/9)).toFixed(1);
+
+  if (converted === 0.0) {
+    return Math.round(Number(converted));
+  }
+  else 
+
+  return Number(converted);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+  let converted = ((celcius * (9/5)) + 32).toFixed(1);
+
+  if (converted === 0.0) {
+    return Math.round(Number(converted));
+  }
+  else 
+  
+  return Number(converted);
+
 };
 
 // Do not edit below this line
