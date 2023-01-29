7395ca188f2c3fb29a29d14843d3120c144a0f31
calforcal <mcalla242@gmail.com>
2023-01-29 08:34:51 -0700

    Upload the solution for 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
+/*
+Get user input of an array and an element to remove for the function
+Remove the element from the array given.
+    Scan the array to identify the elements index in the array
+    Remove the element at that index from the array.
+    Return the new array
+*/
+
 const removeFromArray = function() {
+
+    let array = arguments[0];
+    let index;
+    
+    for (let i = 1; i < arguments.length; i++) {
+        
+        index = array.indexOf(arguments[i]);
+
+        if (index != -1) {
+        array.splice(index, 1);
+        }
+
+    }
+
+    return array;
+
 
 };
 
